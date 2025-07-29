50dc21d834d7469bcb3898968f37812dc944f552
Haymi Haymid <hayminib@gmail.com>
2025-07-29 09:23:46 +0000

    it STAYS ON THIS PAGE AND
    Error: Route "/api/payment/status/[transacti

--- src/app/api/payment/status/[transactionId]/route.ts
+++ src/app/api/payment/status/[transactionId]/route.ts
@@ -14,7 +14,7 @@ export async function GET(
     }
 
     const order = await prisma.pendingOrder.findUnique({
-      where: { transactionId },
+      where: { transactionId: transactionId },
     });
 
     if (!order) {
