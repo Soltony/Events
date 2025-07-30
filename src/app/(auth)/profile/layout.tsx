cfd8bc92e187fbf0cc4903a4c3b0f49088523213
blen Nib <blennib12@gmail.com>
2025-07-30 07:53:16 +0000

    Remove the extra space between the top profile navbar and the Change Pas

--- src/app/(auth)/profile/layout.tsx
+++ src/app/(auth)/profile/layout.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import AuthGuard from '@/components/auth-guard';
+import { UserNav } from '@/components/user-nav';
 
 export default function ProfileLayout({
   children,
@@ -11,7 +12,12 @@ export default function ProfileLayout({
   return (
     <AuthGuard>
         <div className="flex flex-col min-h-screen bg-background">
-            <main className="flex-1 flex items-center justify-center p-4">
+             <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 flex-shrink-0">
+              <div className="flex-1">
+              </div>
+              <UserNav />
+            </header>
+            <main className="flex-1 flex justify-center p-4 pt-8">
                 {children}
             </main>
         </div>
