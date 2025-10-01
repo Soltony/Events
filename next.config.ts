36260377cde21e6fbd0296cab11e1c264e23a445
Haymi <hayminib@gmail.com>
2025-10-01 06:53:00 +0000

    Ensure that your web server, application server, load balancer, etc. is

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  img-src 'self' data: https://placehold.co https://storage.googleapis.com;
+  connect-src 'self' https://nominatim.openstreetmap.org;
+  frame-src 'self';
+`;
+
 const nextConfig = {
   output: 'standalone',
   typescript: {
@@ -23,6 +34,19 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 
