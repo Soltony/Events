2f7b319a2087b56b2cdc9c50f9331b21fde29a66
Haymi <hayminib@gmail.com>
2025-10-02 08:27:50 +0000

    Content Security Policy (CSP) is an added layer of security that helps t

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,5 +1,7 @@
+
 import { Html, Head, Main, NextScript } from 'next/document'
 import type { DocumentProps } from 'next/document'
+import React from 'react';
  
 class MyDocument extends React.Component<DocumentProps & { nonce: string }> {
   render() {
