3ba86db1084f4f2e48e48fddbb49f9a0eb93dd0a
Haymi <hayminib@gmail.com>
2025-10-15 07:34:17 +0000

    its still like this make sure the HttpOnly and Secure attributes to alwa

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -6,5 +6,20 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider 
+      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      storageKey="nib-theme"
+      cookieOptions={{
+        sameSite: "lax",
+        secure: true,
+        httpOnly: true,
+      }}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
