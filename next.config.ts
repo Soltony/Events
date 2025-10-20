c0d04510ab0376b308b2f1593a4430e582ed8dad
blen Nib <hayminib@gmail.com>
2025-10-20 11:36:48 +0000

    Missing key HTTP security headers in next.config.ts. 🔴 Severity: High �

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,18 @@ const securityHeaders = [
       key: 'X-Content-Type-Options',
       value: 'nosniff',
     },
+    {
+      key: 'X-Frame-Options',
+      value: 'DENY',
+    },
+     {
+      key: 'Referrer-Policy',
+      value: 'origin-when-cross-origin',
+    },
+    {
+      key: 'Permissions-Policy',
+      value: "camera=(), microphone=(), geolocation=(), payment=()",
+    }
 ];
 
 const nextConfig: NextConfig = {
