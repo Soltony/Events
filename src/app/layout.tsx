42bf890f26ee0fc753bf502086e4ad950a7764a6
Haymi <hayminib@gmail.com>
2025-11-06 12:32:10 +0000

    when I try to login I am getting internal server 500 on the console {err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import Image from 'next/image';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import PaymentStatusWatcher from '@/components/payment/payment-status-watcher';
 import { AuthProvider } from '@/context/auth-context';
 import { ConditionalFooter } from '@/components/conditional-footer';
 import { headers } from 'next/headers'
@@ -39,8 +38,6 @@ export default async function RootLayout({
         <AuthProvider>
             <div className="flex flex-col min-h-screen relative">
               <main className="flex-1 bg-background">
-                {/** Lightweight global watcher to auto-redirect to confirmation for guests */}
-                <PaymentStatusWatcher />
                 {children}
               </main>
               <ConditionalFooter />
