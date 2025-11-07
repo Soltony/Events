9d27cd71426b772d0d5a881cfc5e73e41e77c7bd
blen Nib <blennib12@gmail.com>
2025-11-07 06:40:33 +0000

    8885-000797012c17c500.js:1  GET https://nibteratickets.nibbank.com.et/ap

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,7 +36,7 @@ export async function middleware(req: NextRequest) {
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com;
     font-src 'self' https://fonts.gstatic.com;
     img-src 'self' data: https://placehold.co https://storage.googleapis.com https://picsum.photos;
