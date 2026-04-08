b03fcd687bf162be16cf1cb3dc2246e11d59409e
blen000 <blenkassahun08@gmail.com>
2026-04-08 14:09:11 +0300

    this is ticket sticky change

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -725,7 +725,7 @@ export default function PublicEventDetailPage() {
                             <Button
                               size="icon"
                               variant="outline"
-                              className="h-9 w-9 -translate-y-[3px]"
+                              className="h-9 w-9"
                               onClick={() => updateTicketQuantity(ticket, Math.min(effectiveMax, selectedQuantity + 1))}
                               disabled={isUnavailable || (typeof effectiveMax === 'number' ? selectedQuantity >= effectiveMax : selectedQuantity >= remaining)}
                             >
